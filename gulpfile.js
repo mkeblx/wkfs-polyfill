186af3e6bc005daf85c13786c0a1d0cd8997901a
Michael Blix <mkeblx@gmail.com>
2019-06-03 15:44:11 -0700

    Log polyfill file size

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
-    log = require('fancy-log');
+    log = require('fancy-log'),
+    size = require('gulp-size');
 
 function defaultTask(cb) {
+  const s = size({pretty: true, showFiles: true});
+
   gulp.src('./src/index.js')
+    .pipe(s)
     .pipe(rename('wkfs.polyfill.js'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
     log('copied to dist/wkfs.polyfill.js');
     cb();
 };
