f66adc9b7f093998278d0ab539ab4282645ff94b
Michael Blix <mkeblx@gmail.com>
2019-06-04 11:59:37 -0700

    Uglify to min file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     log = require('fancy-log'),
-    size = require('gulp-size');
+    size = require('gulp-size'),
+    uglify = require('gulp-uglify');
 
-function defaultTask(cb) {
+function copy(cb) {
   const s = size({pretty: true, showFiles: true});
 
-  gulp.src('./src/index.js')
+  gulp.src('src/index.js')
     .pipe(s)
     .pipe(rename('wkfs.polyfill.js'))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('dist'));
+
     log('copied to dist/wkfs.polyfill.js');
     cb();
 };
 
-exports.default = defaultTask;
+function compress(cb) {
+  gulp.src('dist/wkfs.polyfill.js')
+    .pipe(uglify({
+      compress:{
+        drop_console: true
+      }
+    }))
+    .pipe(rename('wkfs.polyfill.min.js'))
+    .pipe(gulp.dest('dist'));
+
+    log('compressed to dist/wkfs.polyfill.min.js');
+    cb();
+}
+
+var defaultTasks = gulp.series(copy, compress);
+
+exports.default = defaultTasks;
