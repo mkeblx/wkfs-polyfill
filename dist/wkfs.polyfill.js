e069c0a018147f5127767cfd002ddf585d6f4a0a
Michael Blix <mkeblx@gmail.com>
2019-06-04 14:11:44 -0700

    gen

--- dist/wkfs.polyfill.js
+++ dist/wkfs.polyfill.js
@@ -1,50 +1,17 @@
 (function() {
 
-var _logEl = document.getElementById('_log');
-var l = function() {
-  console.log.apply(null, arguments);
-  var text = Array.prototype.join.call(arguments, ' ');
-  _logEl.innerHTML += '<div>'+text+'</div>';
-}
-l('wkfs-polyfill');
-var doc = document ? document : (document = {});
-
-l(doc);
-
-// TODO: move stuff into tests
-// check native
-var fullscreenEnabled = document.fullscreenEnabled !== undefined;
-l('fullscreenEnabled:', fullscreenEnabled);
-
-var requestFullscreen = document.documentElement.requestFullscreen !== undefined;
-l('requestFullscreen:', requestFullscreen);
-
-var exitFullscreen = document.exitFullscreen !== undefined;
-l('exitFullscreen:', exitFullscreen);
-
-// check webkit prefix
-var webkitFullscreenEnabled = document.webkitFullscreenEnabled !== undefined;
-l('webkitFullscreenEnabled:', webkitFullscreenEnabled);
-
-var webkitRequestFullscreen = document.documentElement.webkitRequestFullscreen !== undefined;
-l('webkitRequestFullscreen:', webkitRequestFullscreen);
-
-var webkitExitFullscreen = document.webkitExitFullscreen !== undefined;
-l('webkitExitFullscreen:', exitFullscreen);
-
-
 // polyfill
 if (document.exitFullscreen === undefined) {
   // fullscreenEnabled
-  l('polyfill Document.fullscreenEnabled');
+  console.log('polyfill Document.fullscreenEnabled');
   document.fullscreenEnabled = document.webkitFullscreenEnabled || false;
 
   // requestFullscreen
-  l('polyfill Element.requestFullscreen');
+  console.log('polyfill Element.requestFullscreen');
   Element.prototype.requestFullscreen = Element.prototype.webkitRequestFullscreen || function(){};
 
   // exitFullscreen
-  l('polyfill Document.exitFullscreen');
+  console.log('polyfill Document.exitFullscreen');
   document.exitFullscreen = document.webkitExitFullscreen || function(){};
 }
 
