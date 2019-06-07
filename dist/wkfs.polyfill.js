369473621adf19408514a8fa081ce9a365e6a66e
Michael Blix <mkeblx@gmail.com>
2019-06-07 16:39:03 -0700

    use strings for props

--- dist/wkfs.polyfill.js
+++ dist/wkfs.polyfill.js
@@ -3,28 +3,28 @@
 var d = document;
 
 function handleEvent(eventType, event) {
-  d.fullscreenEnabled = d.webkitFullscreenEnabled || false;
-  d.fullscreenElement = d.webkitFullscreenElement || null;
+  d['fullscreenEnabled'] = d['webkitFullscreenEnabled'] || false;
+  d['fullscreenElement'] = d['webkitFullscreenElement'] || null;
   d.dispatchEvent(new Event(eventType), event.target);
 }
 
 // polyfill
-if (d.exitFullscreen === undefined) {
+if (d['exitFullscreen'] === undefined) {
   // fullscreenEnabled
   console.log('polyfill Document.fullscreenEnabled');
-  d.fullscreenEnabled = d.webkitFullscreenEnabled || false;
+  d['fullscreenEnabled'] = d['webkitFullscreenEnabled'] || false;
 
   // requestFullscreen
   console.log('polyfill Element.requestFullscreen');
-  Element.prototype.requestFullscreen = Element.prototype.webkitRequestFullscreen || function(){};
+  Element.prototype['requestFullscreen'] = Element.prototype['webkitRequestFullscreen'] || function(){};
 
   // exitFullscreen
   console.log('polyfill Document.exitFullscreen');
-  d.exitFullscreen = d.webkitExitFullscreen || function(){};
+  d['exitFullscreen'] = d['webkitExitFullscreen'] || function(){};
 
   // fullscreenElement
   console.log('polyfill Document.fullscreenElement');
-  d.fullscreenElement = d.webkitFullscreenElement;
+  d['fullscreenElement'] = d['webkitFullscreenElement'];
 
   // onfullscreenchange
   console.log('polyfill Document fullscreenchange event');
